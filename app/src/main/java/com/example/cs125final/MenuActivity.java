0d09a7764a7752d9b8377a3631ab09c6e391bf7a
Matt Weiler <mweiler3>
2019-12-09 17:42:36 -0600

    With fixed mainactivity launch

--- app/src/main/java/com/example/cs125final/MenuActivity.java
+++ app/src/main/java/com/example/cs125final/MenuActivity.java
@@ -2,7 +2,10 @@ package com.example.cs125final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button button = findViewById(R.id.buttonDiceRoller);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
